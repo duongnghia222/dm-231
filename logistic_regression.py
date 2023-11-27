f34ff1a16291a8f87d43093ff63ff823b0936eb4
git <duongnghia222>
2023-11-27 17:04:43 +0700

    logistic regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, precision_recall_fscore_support
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Read in DataFrame
 df = pd.read_csv('san_francisco_weather_cleaned.csv')
@@ -17,8 +19,9 @@ target = 'weather_desc'
 X_train, X_test, y_train, y_test = train_test_split(df[features], df[target], test_size=0.2, random_state=42)
 
 # Logistic Regression
-lr_model = LogisticRegression(random_state=42, max_iter=1000)
+lr_model = LogisticRegression(random_state=42, max_iter=10000)
 lr_model.fit(X_train, y_train)
+print(f"Number of iterations: {lr_model.n_iter_}")
 
 # Predictions
 y_pred = lr_model.predict(X_test)
@@ -33,3 +36,14 @@ print(classification_report(y_test, y_pred, zero_division=1))
 
 print("\nConfusion Matrix:")
 print(confusion_matrix(y_test, y_pred))
+
+# Confusion Matrix Heatmap
+cm = confusion_matrix(y_test, y_pred)
+sns.heatmap(cm, annot=True, fmt='g', cmap='Blues', xticklabels=lr_model.classes_, yticklabels=lr_model.classes_)
+plt.xlabel('Predicted')
+plt.ylabel('True')
+plt.title('Confusion Matrix')
+plt.show()
+
+precision, recall, fscore, _ = precision_recall_fscore_support(y_test, y_pred, average='weighted', zero_division=1)
+print(f"Precision: {precision:.2f}, Recall: {recall:.2f}, F1-score: {fscore:.2f}")
